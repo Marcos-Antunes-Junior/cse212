4e36fd8cad679f7da4f347b9d92841f942c01a45
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2025-01-16 19:17:42 -0300

    "Finished W02 Code: Queues"

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,79 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Testing Priority Queue with positive number as priority values
+    // Expected Result: Return Anna
+    // Defect(s) Found: No defects found
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Bob", 2);
+        priorityQueue.Enqueue("Marcos", 50);
+        priorityQueue.Enqueue("Anna", 70);
+        priorityQueue.Enqueue("Tim", 5);
+        priorityQueue.Enqueue("Rafael", 37);
+
+
+        var result = priorityQueue.Dequeue();
+
+        if (String.IsNullOrEmpty(result))
+        {
+            Assert.Fail("The value must not be empty.");
+        }
+
+        Assert.AreEqual("Anna", result);
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Testing Priority Queue with negative number as priority values
+    // Expected Result: Return Tim
+    // Defect(s) Found: No defects found
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Bob", -2);
+        priorityQueue.Enqueue("Marcos", -50);
+        priorityQueue.Enqueue("Anna", -70);
+        priorityQueue.Enqueue("Tim", 5);
+        priorityQueue.Enqueue("Rafael", -37);
+
+
+        var result = priorityQueue.Dequeue();
+
+        if (String.IsNullOrEmpty(result))
+        {
+            Assert.Fail("The value must not be empty.");
+        }
+
+        Assert.AreEqual("Tim", result);
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Testing Priority Queue with some repetead number as priority values
+    // Expected Result: Return Marcos
+    // Defect(s) Found: Returned Tim
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        priorityQueue.Enqueue("Bob", -2);
+        priorityQueue.Enqueue("Marcos", 37);
+        priorityQueue.Enqueue("Anna", -70);
+        priorityQueue.Enqueue("Tim", 37);
+        priorityQueue.Enqueue("Rafael", 37);
+
+
+        var result = priorityQueue.Dequeue();
+
+        if (String.IsNullOrEmpty(result))
+        {
+            Assert.Fail("The value must not be empty.");
+        }
+
+        Assert.AreEqual("Marcos", result);
+    }
+
 }
